33d48a822f1257c87c225c238dd23d48a8756a39
Elisee-des <esabidani@gmail.com>
2023-05-13 16:45:24 +0000

    OK pour le redesign de message

--- src/Controller/Admin/MessageController.php
+++ src/Controller/Admin/MessageController.php
@@ -3,13 +3,15 @@
 namespace App\Controller\Admin;
 
 use App\Repository\MessageRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/admin/message', name: 'app_admin_message_')]
 class MessageController extends AbstractController
 {
-    #[Route('/admin/message', name: 'app_admin_message')]
+    #[Route('/', name: 'liste')]
     public function index(MessageRepository $messageRepository): Response
     {
         $messages = $messageRepository->findAll();
@@ -17,5 +19,15 @@ class MessageController extends AbstractController
         return $this->render('admin/message/index.html.twig', [
             'messages' => $messages
         ]);
+    }
+
+    #[Route('/suppression/{idMessage}', name: 'suppression')]
+    public function suppression($idMessage, MessageRepository $messageRepository, EntityManagerInterface $em): Response
+    {
+        $message = $messageRepository->find($idMessage);
+        $em->remove($message);
+        $em->flush();
+
+        return $this->redirectToRoute('app_admin_message_liste');
     }
 }
